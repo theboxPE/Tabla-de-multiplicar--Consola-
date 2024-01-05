51a670e74db0ca66dea36788c59944c5cd305ad9
theboxpe <perezregaladogabriel7@gmail.com>
2024-01-04 21:59:32 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+int num, resultado = 0;
+
+Console.WriteLine("Ingrese el valor deseado");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("El resultado del valor ingresado " + num + " es:");
+
+for (int i=1; i<=12; i++){
+    resultado = num * i;
+    Console.WriteLine(num + "*"+ i + "="+ resultado);
+}
+
+Console.Read();
